7866e294ac4e44ff072185d9a8d0f152d9ac1436
Samantha-Ruth <sam.haberman@gmail.com>
2022-07-15 16:15:27 -0600

    Server is working and information is seeding

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -7,22 +7,22 @@ const commentData = [
     post_id: 1, 
   },
   {
-    title: 'I totally agree',
+    comment_text: 'I totally agree',
     user_id: 5,
     post_id: 2, 
   },
   {
-    title: 'Not sure about this one... ',
+    comment_text: 'Not sure about this one... ',
     user_id: 4,
     post_id: 3, 
   },
   {
-    title: 'No way, I do not believe it.',
+    comment_text: 'No way, I do not believe it.',
     user_id: 3,
     post_id: 4, 
   },
   {
-    title: 'You go girl!',
+    comment_text: 'You go girl!',
     user_id: 1,
     post_id: 5,
   },
--- server.js
+++ server.js
@@ -7,16 +7,22 @@ const path = require('path');
 // const session = require('express-session');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // connect to front end
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+ 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
