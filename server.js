1596df585f9d8d5e1aed8084374963f2e928111f
Samantha-Ruth <sam.haberman@gmail.com>
2022-07-15 09:28:38 -0600

    having trouble with sequelize and the server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const routes = require('./controllers');
+// import sequelize connection
+const sequelize = require('./config/connection');
+const path = require('path');
+// setup sessions
+// const session = require('express-session');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// connect to front end
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+    });
